f674b5277ded99138862f1dc2ec5f9c02ba198e7
Ahmet Kapusız <ahmetkpz70@gmail.com>
2024-04-21 01:42:31 +0300

    -Ordering Service için Mediator design pattern alt yapısı ve dosyalama sistemi oluşturuldu -ilgili paketler projeye dahil edildi -ilgili sınıflar projeye dahil edildi -sınıfların için dolduruldu ve api yazıp contollerlarla bağlama aşamasına gelindi

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,6 +1,20 @@
+using MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<GetAdressByIdQueryHandler>();
+builder.Services.AddScoped<GetAdressQueryHandler>();
+builder.Services.AddScoped<CreateAdressCommandHandler>();
+builder.Services.AddScoped<UpdateAdressCommandHandler>();
+builder.Services.AddScoped<RemoveAdressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
